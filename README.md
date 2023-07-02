51dd03a1a3fe083f693d526a3c9a5f6687e72c4f
unknown <me.leandrop@gmail.com>
2023-07-02 16:56:30 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+[NPBank](https://dub.sh/npbank)
+# NPBank Website Landing Page - Modern UI/UX website 
+
+This project is a bank website landing page built using React, and Tailwind. It provides a modern and responsive design for showcasing bank's services and attracting potential customers.
+
+## Features
+
+- Responsive layout for seamless viewing on various devices.
+- Clean and intuitive user interface for easy navigation.
+- Interactive elements to engage users and provide a dynamic experience.
+- Integration with React for efficient component-based development.
+- Utilization of Tailwind CSS for rapid styling and customization.
+- Fast development and build process with Vite.
+
+
+<img src='https://i.postimg.cc/cHyqTBXy/npbank.png' alt='carhub1'/>
+<img src='https://i.postimg.cc/YSmK1sNz/npbank2.png' alt='carhub2'/>
---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,26 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Effortlessly explore and uncover an enhanced card deal that surpasses the current
+      one by effortlessly following a straightforward series of steps and actions.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
