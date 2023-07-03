d07c3587b7a223af69dce84cdcf3ebc5a777c1cc
thomasHeitFux <thomas41392@gmail.com>
2023-07-03 02:45:03 -0300

    clients

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,8 +20,8 @@ import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
+import people01 from "./people01.jpg";
+import people02 from "./people02.jpg";
 import people03 from "./people03.png";
 
 export {
